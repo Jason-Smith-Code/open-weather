9b37e43c4fc0b4fe686946433b681d74cf8c1380
Jason Smith <jason.smith.code@gmail.com>
2022-01-21 20:22:12 +0000

    progress made in capturing data from open weather

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  
+  const [appState, setAppState] = useState({
+    loading: false,
+    data: null,
+  });
+
+  // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+  
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=amble&appid=9c386e0118890725b196ccbcd09691e5"
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        setAppState({ loading: false, data: data });
+      });
+  }, [setAppState]);
+
+  const CheckState = () => {
+    if(appState.data != null) {
+      console.log(appState.data.name)
+      return <p>{appState.data.name} </p>
+    }
+  }
+  const dataPresent = appState.data;
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {dataPresent == null ? <p> No data present</p>: <p>{appState.data.name}</p>}
     </div>
   );
 }
-
 export default App;
