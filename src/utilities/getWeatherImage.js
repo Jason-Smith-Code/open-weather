4a5baad86864ca6fedb2bacfeaac43e22487059e
jason-smith-code <jasonsmithcode@gmail.com>
2022-01-26 11:29:07 +0000

    readme edits, and new screen shot photo added

--- src/utilities/getWeatherImage.js
+++
@@ -1,8 +0,0 @@
-import { convertTimestamp } from "./convertTimestamp";
-import { useSelector } from 'react-redux';
-
-export const GetWeatherImage = () => {
-    const reduxData = useSelector((state) => state.weather.data);
-    const currentTime = new Date();
-    const sunset = convertTimestamp(reduxData.sys.sunrise)
-}
